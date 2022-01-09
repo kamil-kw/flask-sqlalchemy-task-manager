71fc427f71a6853b543a9b11688c3204bafcca4b
Kamil Kwiatkowski <kwiatekexc@gmail.com>
2022-01-09 15:03:20 +0000

    Correct2 Heroku's DATABASE_URL

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -12,7 +12,7 @@ app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 if os.environ.get("DEVELOPMENT") == "True":
     app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 else:
-    uri - os.environ.get("DATABASE_URL")
+    uri = os.environ.get("DATABASE_URL")
     if uri.startwith("postgres://"):
         uri = uri.replace("postgres://", "postgresql://",1)
     app.config["SQLALCHEMY_DATABASE_URI"] = uri # heroku
